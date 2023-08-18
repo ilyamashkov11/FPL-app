badda0f56263a6021ef547ac9acb07d887143828
mashkoilya <mashkoilya@ecs.vuw.ac.nz>
2023-08-19 00:26:12 +1200

    encorporated proxy so that the front and back ends can run separately. easier for testing front end functionality

---
+++ src/main/frontend/src/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Index1Page() {
+    return (
+        <div>
+            <h1>This is Index 1</h1>
+            <p>Welcome to the first index page.</p>
+        </div>
+    );
+}
+
+export default Index1Page;
---
+++ src/main/frontend/src/setupProxy.js
@@ -0,0 +1,13 @@
+
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8080', // Change this to your Spring Boot API URL
+      changeOrigin: true,
+    })
+  );
+};
