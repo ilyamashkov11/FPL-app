2797aa04db88551aab798080169e10518f887b11
mashkoilya <mashkoilya@ecs.vuw.ac.nz>
2023-08-19 17:38:22 +1200

    still trying to figure out how jsx and frontend work

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,12 @@
 
 import React, { useEffect, useState } from 'react';
+import Greet from './components/Greet';
+import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('/api/league')
-      .then(response => response.text())
-      .then(data => {
-        setData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      {data && <p>{data}</p>}
+      <Greet />
     </div>
   );
 }
