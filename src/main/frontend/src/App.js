207c676ca87dbc50d2573e289691190466661994
mashkoilya <mashkoilya@ecs.vuw.ac.nz>
2023-08-21 12:47:15 +1200

    beggining practice with css. i suck at it

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,26 +1,11 @@
 
 import React from 'react';
-import Greet from './components/Greet';
-import StateTest from './components/StateTest';
-import ApiContent from './components/ApiContent';
-// import Welcome from './components/Welcome';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" lastname="Elena">
-      <p> this is Greets children </p>
-      <h3> this is an h3 header child </h3>
-      </Greet> 
-
-      <Greet>
-      <h1> this is greet 2</h1>
-        <button></button>
-      </Greet>
-
-      <StateTest></StateTest>
-      <ApiContent></ApiContent>
+      <h1>FPLWizard</h1>
     </div>
   );
 }
